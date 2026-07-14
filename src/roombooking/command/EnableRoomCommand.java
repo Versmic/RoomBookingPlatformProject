4f4014ef6dfa4450004fa9891b54e2f3139f23ef
beyzatataman <beyzatataman@gmail.com>
2026-07-14 14:41:53 -0400

    Create EnableRoomCommand.java

---
+++ src/roombooking/command/EnableRoomCommand.java
@@ -0,0 +1,51 @@
+package roombooking.command;
+
+import roombooking.model.enums.RoomStatus;
+import roombooking.service.RoomService;
+import roombooking.observer.NotificationService;
+
+public class EnableRoomCommand implements Command {
+    private int roomId;
+    private RoomService roomService;
+    private RoomStatus previousStatus;
+    private boolean executed = false;
+    
+    public EnableRoomCommand(int roomId, RoomService roomService) {
+        this.roomId = roomId;
+        this.roomService = roomService;
+    }
+    
+    @Override
+    public boolean execute() {
+        if (!roomService.roomExistsById(roomId)) {
+            return false;
+        }
+        
+        previousStatus = roomService.getRoomStatus(roomId);
+        
+        executed = roomService.enableRoom(roomId);
+        if (executed) {
+            NotificationService.getInstance().sendNotification(
+                "Room enabled: " + roomId
+            );
+        }
+        return executed;
+    }
+    
+    @Override
+    public boolean undo() {
+        if (previousStatus == RoomStatus.AVAILABLE || previousStatus == RoomStatus.OCCUPIED) {
+            return roomService.setRoomStatus(roomId, previousStatus);
+        }
+        return roomService.disableRoom(roomId);
+    }
+    
+    @Override
+    public String getCommandType() {
+        return "ENABLE_ROOM";
+    }
+    
+    public boolean isExecuted() {
+        return executed;
+    }
+}
